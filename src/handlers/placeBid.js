251df8bd59da9a89e18e066806b03cd18e40658c
Iugdor <grodasgomez@gmail.com>
2021-09-29 17:23:10 -0400

    Validation when someone bid on closed auctions

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -11,9 +11,15 @@ async function placeBid(event, context) {
 
   const auction = await getAuctionById(id);
 
+  if(auction.status === 'CLOSED'){
+    throw new createError.Forbidden(`You cannot bid on closed auctions!`);
+  }
+
+
   if (amount <= auction.highestBid.amount) {
     throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}`);
   }
+
 
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
