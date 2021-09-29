28ff7e65df85df5f13bcb210f962d01a6e25bd14
Iugdor <grodasgomez@gmail.com>
2021-09-29 17:59:16 -0400

    Validating getAuctions queryParameters

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,16 +1,30 @@
 import AWS from 'aws-sdk';
 import createError from 'http-errors';
 import commonMiddleware from '../lib/commonMiddleware';
+import validator from '@middy/validator';
+import getAuctionsSchema from '../lib/schemas/getAuctionsSchema';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function getAuctions(event, context) {
   let auctions;
+  const { status } = event.queryStringParameters;
 
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: 'statusAndEndDate',
+    KeyConditionExpression: '#status = :status',
+    ExpressionAttributeValues: {
+      ':status': status,
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status'
+    }
+  };
   try {
-    const result = await dynamodb.scan({
-      TableName: process.env.AUCTIONS_TABLE_NAME,
-    }).promise();
+    const result = await dynamodb.query(params).promise();
     auctions = result.Items;
+
   } catch (error) {
     console.error(error);
     throw new createError.InternalServerError(error);
@@ -21,4 +35,11 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = commonMiddleware(getAuctions);
+export const handler = commonMiddleware(getAuctions)
+  .use(validator({
+    inputSchema: getAuctionsSchema,
+    ajvOptions: {
+      useDefaults: true,
+      strict: false
+    }
+  }));
