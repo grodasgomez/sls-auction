1d5ee74e0b4078ccae27c2f189b878c212b1639b
Iugdor <grodasgomez@gmail.com>
2021-10-04 19:58:31 -0300

    Sending message to the queue

--- src/lib/closeAuction.js
+++ src/lib/closeAuction.js
@@ -1,6 +1,7 @@
 import AWS from 'aws-sdk';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+const sqs = new AWS.SQS();
 
 export async function closeAuction(auction) {
   const params = {
@@ -15,6 +16,30 @@ export async function closeAuction(auction) {
     },
   };
 
-  const result = await dynamodb.update(params).promise();
-  return result;
+  await dynamodb.update(params).promise();
+  const { title, seller, highestBid } = auction;
+  const { amount, bidder } = highestBid;
+
+  const sellerBody = getMessageBody('Your item has been sold!', seller, `Woohoo! Your item "${title}" has been sold for $${amount}.`);
+  const notifySeller = sqs.sendMessage({
+    QueueUrl: process.env.MAIL_QUEUE_URL,
+    MessageBody: sellerBody
+  }).promise();
+
+  const bidderBody = getMessageBody('You won an auction!', bidder, `What a great deal! You got yourself a "${title}" for $${amount}.`);
+
+  const notifyBidder = sqs.sendMessage({
+    QueueUrl: process.env.MAIL_QUEUE_URL,
+    MessageBody: bidderBody
+  }).promise();
+
+  return Promise.all([notifySeller, notifyBidder]);
+}
+
+function getMessageBody(subject, recipient, body) {
+  return JSON.stringify({
+    subject,
+    recipient,
+    body,
+  });
 }
