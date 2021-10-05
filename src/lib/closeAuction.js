5f59cc8aaf888fbe3365af18fbc27ac12af2481f
Iugdor <grodasgomez@gmail.com>
2021-10-05 10:48:50 -0300

    Auction without highest bid validation

--- src/lib/closeAuction.js
+++ src/lib/closeAuction.js
@@ -20,26 +20,32 @@ export async function closeAuction(auction) {
   const { title, seller, highestBid } = auction;
   const { amount, bidder } = highestBid;
 
-  const sellerBody = getMessageBody('Your item has been sold!', seller, `Woohoo! Your item "${title}" has been sold for $${amount}.`);
-  const notifySeller = sqs.sendMessage({
-    QueueUrl: process.env.MAIL_QUEUE_URL,
-    MessageBody: sellerBody
-  }).promise();
+  // If the highest bid amount equals zero, there weren't any bids in the auction
+  if (amount === 0) {
+    // Notify to the seller
+    const request = getMessageRequest('No bids on your auction item :(', seller, `Unfortunately, your item ${title} didn't get any bids!`);
+    await sqs.sendMessage(request).promise();
+    return;
+  }
 
-  const bidderBody = getMessageBody('You won an auction!', bidder, `What a great deal! You got yourself a "${title}" for $${amount}.`);
+  // Otherwise, send an email to the seller and the bidder
+  const sellerRequest = getMessageRequest('Your item has been sold!', seller, `Woohoo! Your item "${title}" has been sold for $${amount}.`);
+  const notifySeller = sqs.sendMessage(sellerRequest).promise();
 
-  const notifyBidder = sqs.sendMessage({
-    QueueUrl: process.env.MAIL_QUEUE_URL,
-    MessageBody: bidderBody
-  }).promise();
+
+  const bidderRequest = getMessageRequest('You won an auction!', bidder, `What a great deal! You got yourself a "${title}" for $${amount}.`);
+  const notifyBidder = sqs.sendMessage(bidderRequest).promise();
 
   return Promise.all([notifySeller, notifyBidder]);
 }
 
-function getMessageBody(subject, recipient, body) {
-  return JSON.stringify({
-    subject,
-    recipient,
-    body,
-  });
+function getMessageRequest(subject, recipient, body) {
+  return {
+    QueueUrl: process.env.MAIL_QUEUE_URL,
+    MessageBody: JSON.stringify({
+      subject,
+      recipient,
+      body,
+    })
+  };
 }
